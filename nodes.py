07965e558bc05930fae3fd5eff2c467d47c3be87
Abdulix <mughnisohail@gmail.com>
2024-12-11 13:01:02 +0500

    webp extention added in save-img class

--- nodes.py
+++ nodes.py
@@ -1536,7 +1536,7 @@ class SaveImage:
                         metadata.add_text(x, json.dumps(extra_pnginfo[x]))
 
             filename_with_batch_num = word.replace("%batch_num%", str(batch_number))
-            file = f"{filename_with_batch_num}_{counter:05}_.jpg"
+            file = f"{filename_with_batch_num}_{counter:05}_.webp"
             img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
             results.append({
                 "filename": file,
